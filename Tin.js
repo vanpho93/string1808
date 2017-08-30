526c8813cb41126b5b3d214a1e746c580465f2f6
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-30 21:26:26 +0700

    es6

---
+++ Tin.js
@@ -0,0 +1,11 @@
+class Tin {
+    constructor(id, title, description, image, link) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.link = link;
+    }
+}
+
+module.exports = Tin;
---
+++ f.js
@@ -0,0 +1,6 @@
+// const { sub, add } = require('./e');
+const sub = require('./e').sub;
+const add = require('./e').add;
+
+console.log(sub(4, 5));
+//https://www.youtube.com/watch?v=LTbnmiXWs2k&list=PL57atfCFqj2h5fpdZD-doGEIs0NZxeJTX
